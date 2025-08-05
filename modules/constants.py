cac6a44a409042c41e1073595b444e7bcf697c19
BMWE25Turbo <jgearthsaver@hotmail.com>
2025-08-05 16:30:30 -0700

    Create constants.py

---
+++ modules/constants.py
@@ -0,0 +1,4 @@
+WAZE_API_URL = "https://www.waze.com/row-rtserver/web/TGeoRSS"
+TWITTER_QUERY_TAGS = ["police", "cop", "speed trap"]
+REDDIT_SUBS = ["r/Portland", "r/OregonCity", "r/Oregon"]
+ALERT_RADIUS_MILES = 1.5
