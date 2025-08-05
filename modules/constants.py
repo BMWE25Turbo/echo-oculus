6c3dbd5b4ac98f876b92fb9cf6126aa8d39127e8
BMWE25Turbo <jgearthsaver@hotmail.com>
2025-08-05 16:49:50 -0700

    Update constants.py

--- modules/constants.py
+++ modules/constants.py
@@ -2,3 +2,11 @@ WAZE_API_URL = "https://www.waze.com/row-rtserver/web/TGeoRSS"
 TWITTER_QUERY_TAGS = ["police", "cop", "speed trap"]
 REDDIT_SUBS = ["r/Portland", "r/OregonCity", "r/Oregon"]
 ALERT_RADIUS_MILES = 1.5
+SCANNER_AUDIO_STREAMS = [
+    "https://example-broadcastify-feed.com/live.mp3"
+]
+
+SCANNER_KEYWORDS = [
+    "pursuit", "officer", "suspect", "traffic stop", "armed", "shots fired",
+    "backup", "fleeing", "high speed", "taser", "code 3", "car accident"
+]
